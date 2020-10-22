b4c14c74c5ed500cd8b01f8e3b4b77f625882b9e
Isidoro Aróstegui <arostegui.isidoro@gmail.com>
2020-10-21 22:21:57 -0300

    Están todas las clases

--- include/Compras.h
+++ include/Compras.h
@@ -1,28 +1,38 @@
 #ifndef COMPRAS_H
 #define COMPRAS_H
+///Cambiar a camell case?????
 
+class Compras{
 
-//class Compras: public Operacion{
-//    protected:
-//    char tipofactura; ///Facturas de dos tipos (A: empresa, B: consumidor final)
-//    int Nrofactura;  ///Numero de factura autonumerico (0000-00000000)
-//    float preciofinal;
-//
-//    public:
-//        virtual ~Compras();
-//        Compras();
-//    void CargarCompras();  /// Carga los datos de las ventas
-//    void MostrarCompras(); /// Muestra los datos de las ventas
-//    void setTipoFact(char); /// Asigna el tipo de factura
-//    void setNrofactura(); /// Asigna el numero de factura autonumerico
-//    void setBonificacion();  /// Asigna la bonificacion que requiera de acuerdo al modo de pago
-//    void setprecioFinal();  /// Calcula el precio aplicando la bonificacion
-//    char getTipoFact();  /// Muestra el tipo de factura
-//    int getNroFactura();  /// Muestra el numero de la factura
-//    float getBonificacion(); ///  Muestra la bonificacion aplicada
-//    float getPrecioFinal();  /// Muestra el precio con la bonificacion aplicada
-//
-//    private:
-//};
+    protected:
+    char tipofactura; ///Facturas de dos tipos (A: empresa)
+    int puntoVta;       ///Representa el numero de venta de la sucursal del proveedor( mayor a 0)
+    int nrofactura;  ///Numero de factura autonumerico (-00000000)
+     int IDproducto;
+     int cantidad;  /// Cantidad del producto
+     float precio;  /// Precio unitario del producto
+     float impuesto;  /// Valor del Impuesto aplicado
+     float total; /// Total a pagar, calculado con el impuesto, el precio y la cantidad
+     ///Producto  mercad;
+
+    public:
+    void CargarCompras();  /// Carga los datos de las ventas
+    void MostrarCompras(); /// Muestra los datos de las ventas
+    void setTipoFact(); /// Asigna el tipo de factura
+    void setnrofactura(); /// Asigna el numero de factura autonumerico
+     void setProducto(); /// Asigna el Id de producto ingresado
+     void setpuntoVta();
+     void setPrecio(); ///Asigna el Precio Unitario del producto
+     void setCantProd(); ///Asigna la cantidad de producto en stock
+     void setImpuesto(); ///Asigna el impuesto
+    char getTipoFact();  /// Muestra el tipo de factura
+    int getNroFactura();  /// Muestra el numero de la factura
+     int getProducto(); /// Muestra el numero de  id del producto
+     int getpuntoVta();
+     int getcantProd(); ///Muestra la cantidad de producto corroborado en setCantProd
+    float getPrecio();  ///Muestra el precio unitario del producto
+    float getImpuesto(); ///Muestra el Impuesto aplicado
+    bool grabarEnDisco();///GRABA EN UN ARCHIVO UN REGISTRO DE TIPO PRODUCTO **HECHA**
+};
 
 #endif // COMPRAS_H
--- src/Ventas.cpp
+++ src/Ventas.cpp
@@ -4,47 +4,65 @@
 #include <cstring>
 #include <conio.h>
 #include <time.h>
-#include "../Include/Ventas.h"
-#include "../Include/Operacion.h"
-
-const char * FILE_VENTAS = "Archivos/Ventas.dat";
+#include "Ventas.h"
+#include "funcionesG.h"
 
 using namespace std;
 
-Ventas::Ventas(){
-    Bonificacion=0;
-    preciofinal=0;
-}
+const char * FILE_VENTAS = "Achivos/Ventas.dat";
+
+//Ventas::Ventas(){
+//    Bonificacion=0;
+//    preciofinal=0;
+//}
 
 void Ventas::CargarVtas(){
-    CargarOp();
-      cout<<"Tipo de factura :";
-      cin>>tipofactura;
-      setTipoFact(tipofactura);
-
+    setTipoFact();
+    setNrofactura();
+     setProducto();
+    setCantProd();
+    setPrecio();
+    setImpuesto();
+    setModoPago();
 }
 
  void Ventas::MostrarVtas(){
-
-     MostrarOp();
      cout<<"Tipo de Factura : "<<tipofactura<<endl;
-     cout<<"Numero: 0000 - "<<getNroFactura()<<endl;
+     cout<<"Numero: 0001-00"<<getNroFactura()<<endl;
+    cout<<"Condiciones de pago : "<<modoPago<<endl;
      cout<<"Bonificacion : "<<getBonificacion()<<endl;
      cout<<"Precio Final a pagar : "<<getPrecioFinal()<<endl;
 
-}
+ }
 
-void Ventas::setNrofactura(){Nrofactura= crearId();}
 
-int Ventas::getNroFactura(){return Nrofactura;}
+ int  Ventas::getNroFactura(){
+     return Nrofactura;
+ }
 
-void Ventas::setTipoFact(char _tipofactura){tipofactura = _tipofactura;}
+ void Ventas::setTipoFact(){
+      cout<<"Tipo de factura : ";
+      cin>>tipofactura;
+ }
 
- char Ventas::getTipoFact(){return tipofactura;}
 
-void Ventas::setBonificacion(){      ///PROBAR CON SWITCH
-	int opc;
-	opc = getModoPago();
+
+ void Ventas::setNrofactura(){
+    Nrofactura= crearId(tipofactura);
+ }
+
+
+ char Ventas::getTipoFact(){
+      return tipofactura;
+ }
+
+ void Ventas::setModoPago(){
+    cout<<"Condiciones de pago : ";
+    cin>>modoPago;
+ }
+
+ void Ventas::setBonificacion(){      ///PROBAR CON SWITCH
+    int opc=getModoPago();
     switch(opc) {
     case  1:
     Bonificacion=0.9;
@@ -55,10 +73,15 @@ void Ventas::setBonificacion(){      ///PROBAR CON SWITCH
     case 3:
     Bonificacion=1;
     break;
-    }
+    } }
+
+    int Ventas::getModoPago(){
+     return modoPago;
 }
 
-float Ventas::getBonificacion(){return Bonificacion;}
+ float Ventas::getBonificacion(){
+       return Bonificacion;
+ }
 
  void Ventas::setprecioFinal(){
      precio=getPrecio();
@@ -66,21 +89,109 @@ float Ventas::getBonificacion(){return Bonificacion;}
         preciofinal= precio*Bonificacion;
  }
 
-float Ventas::getPrecioFinal(){return preciofinal;}
-
-///--------------------- GLOBALES -------------------------
-
-int crearId(){
-    int bytes, cant;
-    FILE *p = fopen("FacturaA.dat", "rb");
-    if (p == NULL){
-        return 1;   ///dado que es el primer registro y no existe el archivo forzamos en nro 1
-    }
-    fseek(p, 0, SEEK_END);
-    bytes = ftell(p);
-    fclose(p);
-    cant = bytes / sizeof(Ventas);
-    cout<<bytes;
-    return cant+1;
+float Ventas::getPrecioFinal(){
+    return preciofinal;
 }
 
+void Ventas::setProducto(){
+     cout<<"Producto : ";
+    cin>>IDproducto;
+}
+
+//void Ventas::setProducto(){
+//    int posicion;
+//    Producto  reg;
+//    cout<<" ID Producto : ";
+//    cin>>IDproducto;
+//    posicion=merca.buscarProd(IDproducto);
+//    reg=obtenerProducto(posicion);
+//    setPrecio(reg.getprecio());
+//    setImpuesto(reg.getIva());
+//}
+
+void Ventas::setPrecio(){
+       cout<<"Precio : ";
+    cin>>precio;
+}
+
+
+void Ventas::setCantProd(){
+       cout<<"Cantidad : ";
+    cin>>cantidad;
+}
+
+void Ventas::setImpuesto(){
+    cout<<"Impuesto : ";
+    cin>>impuesto;
+}
+
+//  float Ventas::getMonto(){
+//  return total;
+//  }
+
+int Ventas::getProducto(){
+    return IDproducto;
+}
+
+
+float Ventas::getPrecio(){
+  return precio;
+}
+
+
+
+int Ventas::getcantProd(){
+   return cantidad;
+}
+
+
+ bool Producto::grabarEnDisco(){
+
+        system("cls");
+        FILE *p;
+        bool chequeo;
+
+        p = fopen(FILE_PRODUCTOS,"ab");
+        if(p==NULL){
+            cout << "Error al abrir el archivo \n";
+            return false;
+        }
+        chequeo = fwrite(this, sizeof(Producto),1,p);
+        if(chequeo==1){
+
+            //msj("Carga exitosa",WHITE,GREEN,130,TEXT_LEFT);
+            //cout << "Registro exitoso";
+            fclose(p);
+            //system("pause");
+            return true;
+        }
+        else{
+            //cout << "El registro no pudo guardarse \n\n";
+            fclose(p);
+            //system("pause");
+            return false;
+        }
+    }
+
+    int crearId(char tipo='B'){
+    int bytes, cant;
+    if(tipo == 'A' || tipo == 'a')  {
+    FILE *p = fopen("FacturaA.dat", "rb");
+     if (p == NULL){
+        return 13;   ///dado que es el primer registro y no existe el archivo forzamos en nro 1
+    }
+      fseek(p, 0, SEEK_END);
+    bytes = ftell(p);
+    fclose(p);
+    }else{
+    FILE *p = fopen("FacturaB.dat", "rb");
+     if (p == NULL){
+        return 18;   ///dado que es el primer registro y no existe el archivo forzamos en nro 1
+    }
+      fseek(p, 0, SEEK_END);
+    bytes = ftell(p);
+    fclose(p);
+     }
+    cant = bytes / sizeof(Ventas);
+    return cant+1;
+}
